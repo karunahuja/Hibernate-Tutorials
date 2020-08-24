7091f7807c71ce84886ccab25b6baad5eaafe73a
karun <karunahuja1998@gmail.com>
2020-08-25 02:53:27 +0530

    Configuring Hibernate in Project

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -1,5 +1,8 @@
 package com.tut;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project Started" );
+        Configuration cfg=new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        
+        SessionFactory factory=cfg.buildSessionFactory();
+        
+        System.out.println(factory);
+      
+        
+        System.out.println(factory.isClosed());
+        
     }
 }
