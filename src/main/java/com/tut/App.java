b2ec4961cf24bec06db366405ceadbc3acedfb23
karun <karunahuja1998@gmail.com>
2020-08-25 04:10:20 +0530

    Saving Image and other type value with hibernate

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -1,5 +1,10 @@
 package com.tut;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -13,7 +18,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Project Started" );
         Configuration cfg=new Configuration();
@@ -26,9 +31,26 @@ public class App
         
         Student st=new Student();
         
-        st.setId(101);
+        st.setId(103);
         st.setName("Karun");
         st.setCity("Sangrur");
+        
+        
+        Address ad=new Address();
+        ad.setStreet("street1");
+        ad.setCity("Delhi");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(123.34);
+        //Reading image
+        
+        FileInputStream fis=new FileInputStream("src/main/java/IMG_20200101_132029.jpg");
+        
+        byte[] data=new byte[fis.available()];
+        
+        fis.read(data);
+        ad.setImage(data);
+        
         
         System.out.println(st);
         
@@ -37,11 +59,14 @@ public class App
         Transaction tx=session.beginTransaction();
         
         session.save(st);
+        session.save(ad);
         
         tx.commit();
        
         
         session.close();
+        
+        System.out.println("Done");
         
     }
 }
