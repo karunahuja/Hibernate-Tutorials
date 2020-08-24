00abf30be47470a09b04fd8c1f58dbed45114549
karun <karunahuja1998@gmail.com>
2020-08-25 03:30:26 +0530

    Hibernate Program storing objects into mysql database

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -1,7 +1,11 @@
 package com.tut;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+
 
 /**
  * Hello world!
@@ -18,9 +22,26 @@ public class App
         SessionFactory factory=cfg.buildSessionFactory();
         
         System.out.println(factory);
-      
-        
         System.out.println(factory.isClosed());
+        
+        Student st=new Student();
+        
+        st.setId(101);
+        st.setName("Karun");
+        st.setCity("Sangrur");
+        
+        System.out.println(st);
+        
+        Session session=factory.openSession();
+        
+        Transaction tx=session.beginTransaction();
+        
+        session.save(st);
+        
+        tx.commit();
+       
+        
+        session.close();
         
     }
 }
