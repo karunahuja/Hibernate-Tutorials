45e1d97484d6e8e5f1046fb3fdb6fa43f9392e03
karun <karunahuja1998@gmail.com>
2020-08-26 01:55:13 +0530

    Mapping with annotations

--- src/main/java/util/Main.java
+++ src/main/java/util/Main.java
@@ -1,13 +1,23 @@
 package util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-HibernateUtil.getSessionFactory().openSession();		
+SessionFactory sessionFactory=HibernateUtil.getSessionFactory();
+
+Session session=sessionFactory.openSession();
+
+
+
 		
-		
+session.close();		
 	}
 
 }
